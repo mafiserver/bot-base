ba1133d4f6e55f7f80a6dbbe2bb0a020981f6262
waki285 <67305123+waki285@users.noreply.github.com>
2021-05-14 18:22:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,31 +6,9 @@ const prefix = process.env.prefix;
 try {
   // GAS(Google Apps Script)からの受信(botの常時起動)
   http.createServer(function(req, res){
-   if (req.method == 'POST'){
-     var data = "";
-     req.on('data', function(chunk){
-       data += chunk;
-     });
-     req.on('end', function(){
-       if(!data){
-         res.end("No post data");
-         return;
-       }
-       var dataObject = querystring.parse(data);
-       console.log("post:" + dataObject.type);
-       if(dataObject.type == "wake"){
-         console.log("Woke up in post");
-         res.end();
-         return;
-       }
-       res.end();
-     });
-   }
-   else if (req.method == 'GET'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Discord Bot is active now\n');
-   }
-  }).listen(3000);
+   res.write("OK");
+   res.end();
+  }).listen(8080);
   client.on("ready", message => {
     console.log("Bot準備完了～");
     client.user.setActivity(process.env.activity, { type: process.env.acttype }); 
